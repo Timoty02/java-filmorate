1e75901f033c60208e80a569d51da7c8f60c0644
Tim <timoty02@yandex.ru>
2024-07-27 19:54:21 +0300

    codestyle: исправить проблемы

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,9 +11,9 @@ ON target."name" = source.genre_name
 WHEN NOT MATCHED THEN
   INSERT ("name") VALUES (source.genre_name);
 
-INSERT INTO "films" ("id", "name", "description", "release_date", "duration", "rating_id")
-SELECT 0, 'Остров проклятых', 'Описание фильма', '2010-01-01', 120, 1
-WHERE NOT EXISTS (SELECT 1 FROM "films" WHERE "id" = 0);
+--INSERT INTO "films" ("id", "name", "description", "release_date", "duration", "rating_id")
+--SELECT 0, 'Остров проклятых', 'Описание фильма', '2010-01-01', 120, 1
+--WHERE NOT EXISTS (SELECT 1 FROM "films" WHERE "id" = 0);
 
 MERGE INTO "rating" AS target
 USING (VALUES ('G'), ('PG'), ('PG-13'), ('R'), ('NC-17')) AS source (rating_name)
